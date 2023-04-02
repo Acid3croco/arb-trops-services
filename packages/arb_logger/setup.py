f97ac6cfcd812fa5ff5b4af0c637cbff740280f0
Acid3croco <yoann.ditte@gmail.com>
2023-04-02 09:47:34 +0200

    feat (arb_logger): restore setup.py

---
+++ packages/arb_logger/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arb_logger',
+    author='arb',
+    author_email='arb.trops@gmail.com',
+    description="A custom logger with Redis integration",
+    url="https://github.com/Acid3croco/arb-trops-services",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    version='2.0.1',
+    packages=find_packages(),
+    install_requires=['coloredlogs', 'redis', 'pync'],
+    entry_points={
+        'console_scripts': [
+            'arb_alerts = arb_logger.redis_log_client:main',
+        ]
+    },
+)
