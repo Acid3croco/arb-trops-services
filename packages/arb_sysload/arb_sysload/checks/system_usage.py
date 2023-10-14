5491f27d6ce574b68cf2829d7b3909edb3977510
Jack <yoann.ditte@gmail.com>
2023-10-14 16:12:48 +0000

    feat (arb_sysload): add system_usage check

---
+++ packages/arb_sysload/arb_sysload/checks/system_usage.py
@@ -0,0 +1,30 @@
+import psutil
+
+from arb_sysload.base_check import BaseCheck
+
+
+class SystemUsageCheck(BaseCheck):
+    INTERVAL = 15  # Run every 15 seconds
+
+    def run(self):
+        # CPU usage
+        cpu_percent = psutil.cpu_percent(interval=1)
+        # Load average
+        load_avg_1, load_avg_5, load_avg_15 = psutil.getloadavg()
+        # RAM usage
+        ram = psutil.virtual_memory()
+
+        # Construct the message
+        message = ' - '.join([
+            f"CPU: {cpu_percent}%",
+            f"LA: {load_avg_1:.2f} {load_avg_5:.2f} {load_avg_15:.2f}",
+            f"RAM: {ram.percent}%"
+        ])
+
+        # In this example, we'll assume that a CPU usage of over 90% or RAM usage over 90% is an error.
+        if cpu_percent > 90 or ram.percent > 90:
+            self.error(message)
+        elif cpu_percent > 80 or ram.percent > 80:
+            self.warning(message)
+        else:
+            self.success(message)
